afb71f716d219fb2caf07e29a6067c5bc87b3f62
Manuel Maisano <manumaisano@gmail.com>
2024-06-12 23:03:40 +0200

    firdt

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Type;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $types = ['Frontend', 'Backend', 'Fullstack'];
+        Foreach($types as $type){
+          $new_type = new Type();
+          $new_type->name = $type;
+          $new_type->slug = Type::generateSlug($new_type->name);
+          $new_type->save();
+        }
+    }
+}
